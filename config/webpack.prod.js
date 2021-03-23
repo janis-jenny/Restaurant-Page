632a5b62474b63e54084872f6fcadd623bb70bab
janis-jenny <pcardenasvm@gmail.com>
2021-03-23 17:41:16 -0500

    set up webpack w a template and linters

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,20 +1,20 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const base = require("./webpack.base");
-const { merge } = require("webpack-merge");
+const { merge } = require('webpack-merge');
+const base = require('./webpack.base');
 
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "app.[contenthash].js",
-    assetModuleFilename: 'images/[name][ext][query]'
+    filename: 'app.[contenthash].js',
+    assetModuleFilename: 'images/[name][ext][query]',
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name][contenthash].css"
-    })
+      filename: '[name][contenthash].css',
+    }),
   ],
   module: {
     rules: [
@@ -22,15 +22,15 @@ const prodConfig = {
         test: /.(scss)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
+          'css-loader',
+          'sass-loader',
+        ],
       },
-    ]
+    ],
   },
   optimization: {
     splitChunks: {
-      chunks: 'all'
+      chunks: 'all',
     },
     minimize: true,
     minimizer: [
@@ -39,8 +39,8 @@ const prodConfig = {
       new TerserPlugin({
         extractComments: false,
       }),
-    ]
-  }
-}
+    ],
+  },
+};
 
-module.exports = merge(base, prodConfig)
+module.exports = merge(base, prodConfig);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import "./scss/styles.scss"
+import './scss/styles.scss';
 
 const container = document.querySelector('.joke-container');
 
 const requestJoke = async () => {
-  const url = "https://api.chucknorris.io/jokes/random";
+  const url = 'https://api.chucknorris.io/jokes/random';
   const response = await fetch(url);
   const json = await response.json();
   return json;
-}
+};
 
 const displayJoke = async () => {
   const joke = await requestJoke();
-  container.innerText = joke.value
-}
+  container.innerText = joke.value;
+};
 
-displayJoke()
+displayJoke();
