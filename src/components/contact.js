bd6df8ef5fd22cc24ca2a13947d70cdbb28c1119
janis-jenny <pcardenasvm@gmail.com>
2021-03-30 17:11:13 -0500

    add contact info and reservation on Contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,29 +9,54 @@ export default class Contact {
   title() {
     const title = document.createElement('h1');
     title.classList.add('title-contact')
-    title.innerText = 'Contact';
+    title.innerText = 'CONTACT';
     this.section.append(title);
   }
 
-  /* info(subtitle, imgs) {
-    const cards = document.createElement('div');
-    cards.classList.add('card');
-    const imgcontainer = document.createElement('div');
-    imgcontainer.classList.add('card-img');
-    const cardSubtitle = document.createElement('h3');
-    cardSubtitle.innerText = `${subtitle}`;
-    cardSubtitle.classList.add('card-title');
+  info() {
+    const info = document.createElement('div');
+    info.classList.add('info');
+    const address = document.createElement('p');
+    address.classList.add('address');
+    address.innerText = 'Av. Pedro de Osma 301, Barranco, Lima, Perú.'
+    const phone = document.createElement('p');
+    phone.classList.add('address');
+    phone.innerText = '+51 1 242-8515';
+    const email = document.createElement('p');
+    email.classList.add('email');
+    email.innerText = 'reservas@centralrestaurante.com.pe';
 
-    imageArray(imgcontainer, imgs);
+    info.append(address);
+    info.append(phone);
+    info.append(email);
 
-    cards.append(cardSubtitle);
-    cards.append(imgcontainer);
+    this.section.append(info);
+  }
 
-    this.section.append(cards);
-  } */
+  reservations() {
+    const reserv = document.createElement('div');
+    reserv.classList.add('reservtions');
+    const titlereserv = document.createElement('h3');
+    titlereserv.classList.add('title-reserv');
+    titlereserv.innerText = 'RESERVATIONS:';
+    const days = document.createElement('h4');
+    days.classList.add('days');
+    days.innerText = 'Monday to Saturday';
+    const hours = document.createElement('p');
+    hours.classList.add('days');
+    hours.innerText = 'From 12:00 to 18:30';
+
+    reserv.append(titlereserv);
+    reserv.append(days);
+    reserv.append(hours);
+
+    this.section.append(reserv);
+  }
 
   renderContact() {
     this.title();
+    this.info();
+    this.reservations();
     this.container.append(this.section);
    /*  this.dishes('Snacks', this.snacks);
     this.dishes('Main', this.maindish);
